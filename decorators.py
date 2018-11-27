76d0ff66c1d8a5a89fe30d0659151f196a1aa4f8
Kostiantyn Hantsov <k.gantsov@gmail.com>
2018-11-27 21:44:06 +0100

    Some refactoring

--- decorators.py
+++ decorators.py
@@ -1,16 +1,12 @@
 import functools
 import json
-from flask import request
 
+from flask import request
 from opentracing.ext import tags
 from opentracing.propagation import Format
 
 
 def trace(tracer, span_name):
-    """
-    This decorator generates a JSON response from a Python dictionary or
-    a SQLAlchemy model.
-    """
     def decorator(f):
         @functools.wraps(f)
         def wrapped(*args, **kwargs):
